d1af4cc5075fb48111913841a407890c74f306ca
Your Name <andywang947@gmail.com>
2024-08-22 23:08:43 +0800

    show the classification result on web.

--- backend/classification_model/vgg16.py
+++ backend/classification_model/vgg16.py
@@ -1,17 +1,21 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
-from torchvision import datasets
-from torch.utils.data import DataLoader
-from torchvision import models
+# from torchvision import models
+from torchvision.models import vgg16, VGG16_Weights
+
 import torch.nn as nn
-import torch.optim as optim
+import os
 
 
-VGG16 = models.vgg16(pretrained=True)
+# VGG16 = models.vgg16(pretrained=True)
+VGG16 = vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
+
 num_features = VGG16.classifier[6].in_features
 VGG16.classifier[6] = nn.Linear(num_features, 6)
 
-model_weight_path = r"backend\classification_model\model_weights.pth"
-model_weight = torch.load(model_weight_path, map_location=torch.device('cpu'))
-VGG16.load_state_dict(model_weight)
+weight_path = r"backend\classification_model\model_weights.pth"
+# print(os.path.exists(weight_path))
+if os.path.exists(weight_path):
+    model_weight = torch.load(weight_path, map_location=torch.device('cpu'))
+    VGG16.load_state_dict(model_weight)
+else :
+    print("You didn't put the weight file in the model dictionary. And now will use the original VGG16 didn't trained.")
